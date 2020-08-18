45a5500077851c67610bfea89c2d92197ad3c313
davidmitchell2019 <50448608+davidmitchell2019@users.noreply.github.com>
2020-08-18 23:48:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,34 +5,37 @@ from json import dumps
 
 sourceFile = None
 destFile = None
+#TODO add some error handling
 
+#initialize the files and and take them in as dictionaries
 def init():
-    print("Initializing files and dictionaries")
     global sourceFile, destFile
+    
     file1 = open(r'test.yml')
     file2 = open(r'test2.yml')
+    
     destFile = yaml.load(file1, Loader=yaml.FullLoader)
     sourceFile = yaml.load(file2, Loader=yaml.FullLoader)
 
 # reading a level of the destination file for keys and sub dictionaries
 def replace_level(level):
-    #print(level)
+    #Iterate through the dictionaries and compare the keys
     for key in level:
         if isinstance(level[key], dict):
             level[key] = replace_level(level[key])
         elif key in sourceFile:
             level[key] = sourceFile[key]
-        #elif level[key] == None:
-        #    level[key] = ""
-    #print(level)
     return level
 
 
 def main():
     global destFile
     init()
+    #substitute the values
     destFile = replace_level(destFile)
+    #dump the yaml file
     result = yaml.safe_dump(destFile, open(r'out.yml', 'w'), default_flow_style=False)
+    #print the result to the terminal
     print(result)
 
 main()
